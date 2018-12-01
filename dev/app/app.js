aca21d08c91b2888f3262fdf535101deb392385a
Mohamed Bouanane <mohamedbouanane@MacBook-Pro-de-Mohamed.local>
2018-12-01 20:31:30 +0100

    Dockerfile: gogs jenkins nexus

--- dev/app/app.js
+++ dev/app/app.js
@@ -4,6 +4,10 @@ const port = 3000
 
 app.set('port', port)
 
+app.get('/', (req, res) => {
+  res.send(`Page For Testing Docker`)
+})
+
 app.get('/:name', (req, res) => {
   res.send(`Hola ${req.params.name}!`)
 })
